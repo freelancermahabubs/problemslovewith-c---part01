6a3ed85660819e666b6f8453b56d2386a2dad8db
mahabube <info.freelancermahabub@gmail.com>
2024-04-01 21:54:24 +0600

    first commit

---
+++ problem_3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+
+    int num;
+    if(num  % 2 == 0){
+        printf("even\n");
+    }
+    else{
+        printf("odd\n");
+    };
+
+return 0;
+}
---
+++ problem_5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(){
+    int TK;
+    scanf("%d", &TK);
+
+
+if(TK >= 10000){
+printf("Gucci Bag\n");
+
+if (TK > 20000)
+{
+   printf("Gucci Belt\n");
+}
+ }
+
+  else if (TK >= 5000)
+{
+   printf("Levis Bag\n");
+}
+   
+   else{
+    printf("Something\n");
+}
+    
+    
+return 0;
+}
